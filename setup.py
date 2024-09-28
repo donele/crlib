3088c22cfb13e90818ed51e63593f8468b3c7ae1
Jedong Lee <dxdy@yahoo.com>
2024-09-28 09:52:26 -0400

    Minor changes

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 from Cython.Build import cythonize
 setup(
-    ext_modules=cythonize(['crlib2/crfeatures.py', 'crlib2/crlinreg.py', 'crlib2/crtreereg.py'])
+    ext_modules=cythonize([
+        'crlib2/crfeatures.py',
+        'crlib2/crlinreg.py',
+        'crlib2/crtreereg.py',
+    ])
 )
