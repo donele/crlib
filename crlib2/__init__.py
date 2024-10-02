228ae4fc02702a4e34b6511dbad5ff5c0d2d919a
Jedong Lee <dxdy@yahoo.com>
2024-10-01 21:49:37 -0400

    Import all the files contents

--- crlib2/__init__.py
+++ crlib2/__init__.py
@@ -1,4 +1,16 @@
 from . import crlib2
+from . import crdataio
+from . import crfeatures
+from . import crlinreg
+from . import crpaths
+from . import crtreereg
+
 from .crlib2 import *
+from .crdataio import *
+from .crfeatures import *
+from .crlinreg import *
+from .crpaths import *
+from .crtreereg import *
+
 from .fit1d import *
 from .write_features import *
